ed506014e6156b3a08045a9a1571c2a400977983
CCwong <chongchen.wong@atoz-software.tech>
2023-10-24 17:43:54 +0800

    skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { CustomSkeleton } from './components/Custom';
 
 const App: React.FC = () => {
   return (
     <div className='mx-8'>
       <p className='text-red-500 text-[50px]'>Appahjksdghakjdshfjka</p>
-
     </div>
   );
 };
